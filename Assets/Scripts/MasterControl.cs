17d34b4ef44c4925200e92238c94c8c87c26981f
ming17 <mci10@pitt.edu>
2019-07-24 10:33:23 -0400

    Updated Camera Animations

---
+++ Assets/Scripts/MasterControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterControl : MonoBehaviour
+{
+    public Animator m_CameraMotion;
+    public ParticleSystem m_Water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Next()
+    {
+        m_CameraMotion.SetBool("Next", true);
+    }
+
+    public void ActivateWater()
+    {
+        m_Water.Play();
+    }
+
+    public void DeactivateWater()
+    {
+        m_Water.Stop();
+    }
+}
